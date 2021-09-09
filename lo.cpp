68ab976011a880d72e09a70daac084baa64ebd2c
outsider1113 <56751350+outsider1113@users.noreply.github.com>
2021-09-09 07:42:49 -0700

    ok

--- lo.cpp
+++ lo.cpp
@@ -1,17 +1,32 @@
 #include<iostream>
 #include<string>
 #include<fstream>
+#include<iomanip>
 using namespace std;
 
 void printMenu();
+void getData(ifstream &infile, record validRecords[], record invalidRecords[]);
 int main() {
+
     struct record{
-        int id;
+        string id;
         string name;
         int quantity;
         double price;
     };
+    const int MAX = 100;
+
+    record validRecords[MAX];
+    record invalidRecords[MAX];
+
     ifstream infile;
+    infile.open("allRecords.txt");
+
+    if(!infile){
+    cout << "Error opening infile";
+    exit(1);
+    }
+
     ofstream outfile;
     printMenu();
     
@@ -24,6 +39,7 @@ int main() {
     switch(num) {
         case printAll:
         cout << "Option 1";
+        getData(infile, validRecords, invalidRecords)
         break;
         case printInvalid:
         cout << "Option 2";
@@ -39,9 +55,17 @@ return 0;
 void printMenu(){
     cout<< "Invetory Organizer:\n\nEnter the number that correlates to an option\n\n1: Print all current items\n2: Print invalid Records\n3: Quit\n\nPlease enter an option: ";
 }
-void getData(ifstream &infile){
-    
+
+void getData(ifstream &infile, record valRecords[], record invalRecords[]){
+    int counter = 0;
+    while (!infile.eof()){
+        infile >> valRecords[counter].id;
+        infile >> valRecords[counter].name;
+        infile >> valRecords[counter].quantity;
+        infile >> valRecords[counter].price;
+        counter++;
+    }
+
 }
 
 
-void addNumFromUser(int userNum, int overallNum){}
